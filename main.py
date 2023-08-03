e03fd6cd2b92f879a6c010b23cae2a1d217941b3
Xoelelproaso <138804188+Xoelelproaso@users.noreply.github.com>
2023-08-03 19:46:04 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
-import discord
+import discord,os,random
+from discord.ext import commands
 
-from bot_logic import gen_pass
-
-# La variable intents almacena los privilegios del bot
 intents = discord.Intents.default()
-# Activar el privilegio de lectura de mensajes
-intents.message_content = True
-# Crear un bot en la variable cliente y transferirle los privilegios
-client = discord.Client(intents=intents)
+intents.message_content=True
 
-@client.event
+bot=commands.Bot(command_prefix="$",intents=intents)
+
+@bot.event
 async def on_ready():
-    print(f'Hemos iniciado sesión como {client.user}')
+    print(f"has iniciado sesión como {bot.user}")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send("Hi!")
-    elif message.content.startswith('$bye'):
-        await message.channel.send("See you soon!")
-    elif message.content.startswith('$password'):
-        await message.channel.send("Your password " + gen_pass(10))
+@bot.command()
+async def meme(ctx):
+    imagenes_cargadas=random.choice(os.listdir("Clase 6\memes"))
+    with open(f'Clase 6\memes\{imagenes_cargadas}', 'rb') as f:
+        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en esta variable!
+        picture = discord.File(f)
+    # A continuación, podemos enviar este archivo como parámetro.
+    await ctx.send(file=picture)
 
-client.run("MTEyOTA5MjU1MTc2MTIxOTcyNg.GKijTX.cMX294Qvjj3ItVUrhoqAkJCk_jShImniuT7xBg")
+@bot.command()
+async def reciclar(ctx):
+    await ctx.send("Contenedor amarillo: Dentro del contenedor de color amarillo debemos depositar botellas y envases de plástico, envases metálicos (latas, bandejas de aluminio, aerosoles, botes de desodorante…) y briks. Es decir, es el contenedor perfecto para tirar ENVASES.\n\nContenedor azul: El contenedor azul es el destinado para envases de cartón y papel exclusivamente. Algunos de estos productos pueden ser envases de alimentación, calzado, productos congelados, papel para envolver o papel de uso diario.\n\nContenedor verde: En el contenedor verde debemos depositar botellas de vidrio, frascos de vidrio (como perfumes o colonias) y tarros de alimentos (mermeladas, conservas, etc).\n\nContenedor marrón (orgánico): En este contenedor deben depositarse los restos de alimentos como pieles de frutas, espinas de pescado, plantas, cáscaras de huevo o posos; o servilletas y papel de cocina usados.\n\nContenedor gris (restos): En este contenedor se depositan materiales como juguetes, biberones, chupetes, utensilios de cocina, pañales, objetos cerámicos, arena para mascotas, pelo, polvo, colillas, etc. Es decir, todo aquello que no puede ser reutilizado.")
+
+@bot.command()
+async def commands(ctx):
+    await ctx.send("$reciclar\n$meme\n")
+
+bot.run("Token")
